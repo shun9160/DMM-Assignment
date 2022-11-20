135a5d5438f48fbb7fd11ab43291bc39a80fe739
shun koshio <n01463364@humbermail.ca>
2022-11-19 19:48:17 -0500

    最終コミット

---
+++ 学習/img/shun.js
@@ -0,0 +1,14 @@
+// alert hello when you open the page
+
+function loadHello(){
+    alert("Hello!");
+  }
+
+
+// simple math 
+  function sum(){
+    let x = 3;
+    let y = 4;
+    let z = x + y;
+    alert(z);
+  }
---
+++ 学習/img/shun.rb
@@ -0,0 +1,14 @@
+# In Ruby -----------------------------------------------------------------------------------------------------
+
+# No need for variable declarations
+# No need for semicolons at the end of sentences
+# The way to express a chunk of processing is not to enclose it with "function 〜(){}", but with "def" and "end
+
+# --------------------------------------------------------------------------------------------------------------
+
+def sum
+    x = 3
+    y = 4
+    z = x + y
+    p z
+  end
